d0f9952dc5c739f1a47cc94b28f651f002dd6b89
yuliiakandych <y_kandych@fanshaweonline.ca>
2018-10-23 13:55:00 -0400

    Game

--- RPSGame.py
+++ RPSGame.py
@@ -4,9 +4,18 @@ from random import randint
 #choices is an array => a container that can hold multiple items
 #arrays are 0-based -> the first item is 0, the second is 1, etc
 choices = ["Rock", "Paper", "Scissors"]
+player = False
 
 #make the computer choose a weapon from the choices array at random
-computer_choice = choices[randint(0,2)]
+computer_choice = choices[randint(0, 2)]
 
 #print the choise to the terminal window
 print("Computer chooses: ", computer_choice)
+
+# set up our loop
+while player is False:
+    # set player to True by making a selection
+    print("Choose your weapon!")
+    player = input("Rock, Paper or Scissors?")
+
+    print(player, "\n")
--- conditions.py
+++ conditions.py
@@ -5,6 +5,7 @@ print("Rules that govern the states of water")
 current_temp = False
 
 while current_temp is False:
+	# make this a number and clean the code up (DRY it out)
     current_temp = input("Enter a temperature:\n")
     #see what current temp is
     print("you input:", current_temp)
@@ -19,6 +20,6 @@ while current_temp is False:
     	current_temp = False
 
     elif (int(current_temp) > 100 or (int(current_temp) == 100)):
-    	print("water is a gas. Cuz it's, like, really hot")
+    	print("water is a gas. cuz it's, like, really hot")
     	current_temp = False
 
