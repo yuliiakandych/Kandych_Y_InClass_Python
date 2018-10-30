6afb1ec248836d21a78e67ba2ddbe2d3447a27d8
yuliiakandych <y_kandych@fanshaweonline.ca>
2018-10-30 19:46:04 -0400

    InClassWinLose

--- RPSGame.py
+++ RPSGame.py
@@ -1,100 +1,102 @@
 # import the random package so that we can generate random numbers
 from random import randint
 
-player_wins = 5 
-computer_wins = 5
 # choices is an array => a container that can hold multiple items
 # arrays are 0-based -> the first item is 0, the second is 1, etc
 choices = ["Rock", "Paper", "Scissors"]
 player = False
 
-print("""
-Player Wins:{}
-Computer Wins:{}""".format(player_wins, computer_wins))
+player_lives = 5
+computer_lives = 5
 
 # make the computer choose a weapon from the choices array at random
-computer_choice = choices[randint(0, 2)]
+computer = choices[randint(0, 2)]
 
-# print the choise to the terminal window
-print("Computer chooses: ", computer_choice)
+# def win or lose function
+
+
+def winorlose(status):
+    print("Called win or loose function")
+    print("***********************************")
+    print("you", status, " ! Would you like to play again")
+    choice = input ("Y / N:")
+
+    #reset the lives
+    if choice == "Y" or choice == "y":
+        #change global variables
+        global player_lives
+        global computer_lives
+        global player
+        global computer
+
+        player_lives = 5
+        computer_lives = 5
+        player = False
+        computer = choices[randint(0, 2)]
+
+    elif choice == "N" or choice == "n":
+        print("You chose to quit")
+        print("***********************************")
+        exit()
+
+# showe the computer's choice to the terminal window
+print("computer chooses: ", computer)
 
 # set up our loop
 while player is False:
+    print("=====================================")
+    print ("player lives", player_lives, "/5")
+    print ("computer lives", computer_lives, "/5")
+    print("=====================================")
     # set player to True by making a selection
-    print("Choose your weapon!")
+    print("Choose your weapon!\n")
     player = input("Rock, Paper or Scissors?\n")
-
-    print(player, "\n")
-
+    print("player chooses:", player)
 
     # check for a tie = choices are the same
-    if player == computer_choice:
+    if (player == computer):
         print("Tie! You live to shoot another day")
 
     # check to see if the computer choice beats our choice or not
     elif player == "Rock":
         if computer_choice == "Paper":
             # computer won. crap!
-            print("You lose! Copmuter chooses:", computer_choice)
-            player_wins -=1
+            player_lives = player_lives - 1
+            print("You lose!", computer, "covers", player)
         else:
             # we win! such winning
-            print("You win! Copmuter chooses:", computer_choice, player, "smashes", computer_choice)
-            computer_wins -=1
+            computer_lives = computer_lives - 1
+            print("You win!", player, "smashes", computer)
 
     elif player == "Paper":
         if computer_choice == "Scissors":
-            print("You lose! Copmuter chooses:", computer_choice, computer_choice, "cut", player)
-            player_wins -=1
+            player_lives = player_lives - 1
+            print("You lose!", computer_choice, "cut", player)
+
         else:
-            print("You win! Copmuter chooses:", computer_choice, player, "covers", computer_choice)
-            computer_wins -=1
+            computer_lives = computer_lives - 1
+            print("You win!", player, "covers", computer)
 
     elif player == "Scissors":
         if computer_choice == "Rock":
-            print("You lose! Copmuter chooses:", computer_choice, computer_choice, "smashes", player)
-            player_wins -=1
+            player_lives = player_lives - 1
+            print("You lose", computer, "smashes", player)
         else:
-            print("You win! Copmuter chooses:", computer_choice, player, "cut", computer_choice)
-            computer_wins -=1
+            computer_lives = computer_lives - 1
+            print("You win", player, "cut", computer)
     elif player == "quit":
         exit()
+
     else:
         print("Check your spelling... that's a not a valid choice\n")
 
-    print("""
-Player Wins:{}
-Computer Wins:{}""".format(player_wins, computer_wins))
+    # check for win or lose
+    if player_lives is 0:
+        winorlose("lose")
+
+    if computer_lives is 0:
+        winorlose("won")
 
     # reset the game loop and start over again
-
-
-    if player_wins == 0:
-        print("Computer won the game")
-        play_again = input("Would you like to play again? (Y/N) >")
-        if (play_again == "Y"):
-            player_wins = 5
-            computer_wins = 5
-            continue
-        else:
-            exit()
-
-    elif computer_wins == 0:
-        print("You won the game")
-        play_again = input("Would you like to play again? (Y/N) >")
-        if (play_again == "Y"):
-            player_wins = 5
-            computer_wins = 5
-            continue
-        else:
-            exit()
-
-    if player_wins > computer_wins:
-        print ("You're winning!")
-    elif player_wins < computer_wins:
-        print ("You're in trouble, you're losing")
-    else:
-        print("The game is tied")
-
     player = False
     computer_choice = choices[randint(0, 2)]
--- function.py
+++ function.py
@@ -3,6 +3,34 @@ def greetings():
 
 
 
-# the greetings function say hello
-# invoke 
+# the greetings function just says hello
+# invoke or call the function
 greetings()
+
+def greetingsWithArgs(msg="a default message"):
+    # print the msg variable
+    print("now we're saying", msg, "from another function")
+
+
+greetingsWithArgs()
+greetingsWithArgs("somethin completely different")
+greetingsWithArgs("running yet again")
+
+# variables and scopes
+myNumberVariable = 10
+
+
+# returning values from functions (very powerful)
+def someMath(num1=2, num2=4):
+    global myNumberVariable
+
+    myNumberVariable = num1 + num2
+    return num1 + num2
+
+
+sum = someMath()
+print("we are doing some math an getting", sum)
+
+sum = someMath(10, 15)
+print("another math operation with", sum, "as the result")
+
